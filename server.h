e9403db1f2c8bf4ef5acfaf48c34a30f3b6c7034
Ningyuan Wang <nywang@google.com>
2016-09-09 13:15:37 -0700

    Broadcast interface changes through binder
    This broadcasts interface ready/towndown events after we
    create/remove an interface.
    
    BUG=31349441
    TEST=compile, unit tests
    Change-Id: I8d4f20ddb82db1bb53d12f3feeb06d4e761be689

--- server.h
+++ server.h
@@ -87,6 +87,12 @@ class Server : public android::net::wifi::BnWificond {
                              uint32_t* interface_index,
                              std::vector<uint8_t>* interface_mac_addr);
   bool RefreshWiphyIndex();
+  void BroadcastClientInterfaceReady(
+      android::sp<android::net::wifi::IClientInterface> network_interface);
+  void BroadcastApInterfaceReady(
+      android::sp<android::net::wifi::IApInterface> network_interface);
+  void BroadcastClientInterfaceTornDown();
+  void BroadcastApInterfaceTornDown();
 
   const std::unique_ptr<wifi_system::HalTool> hal_tool_;
   const std::unique_ptr<wifi_system::InterfaceTool> if_tool_;
