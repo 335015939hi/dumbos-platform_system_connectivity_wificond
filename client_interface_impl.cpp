83d57a8a28274665db07b89bb38034e585a1e6be
Ningyuan Wang <nywang@google.com>
2016-09-14 13:47:38 -0700

    Add getMacAddress() to wificond binder interface
    Bug: 31042306
    Test: compile, integration test
    
    Change-Id: Ic8b510aa761b4e79bf903f2d90af680b7cb5ca24

--- client_interface_impl.cpp
+++ client_interface_impl.cpp
@@ -88,6 +88,10 @@ bool ClientInterfaceImpl::GetPacketCounters(vector<int32_t>* out_packet_counters
   return true;
 }
 
+const vector<uint8_t>& ClientInterfaceImpl::GetMacAddress() {
+  return interface_mac_addr_;
+}
+
 void ClientInterfaceImpl::OnScanResultsReady(
                          uint32_t interface_index,
                          std::vector<std::vector<uint8_t>>& ssids,
