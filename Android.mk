0210595396ecb373def1a92896ab5f33aab7e307
Ningyuan Wang <nywang@google.com>
2016-09-27 09:41:12 -0700

    Add Rtt interfaces to wificond part 1
    This adds a parcelable object RttParams, which is supposed to
    match class ParcelableRttParams in RttManager.java
    
    Later we will use ParcelableRttParams for framework to interact
    with wificond, so this is the c++ side definition of this object.
    
    This adds requestRttRange() and cancelRtt(Range) to IClientInterface.
    This also adds a dummy definition of interface IRttEventCallback.
    
    Bug: 31756849
    Test: compile
    
    Change-Id: I457c910adb8b1e2f4668cc24e87070560df5396c

--- Android.mk
+++ Android.mk
@@ -93,9 +93,11 @@ LOCAL_C_INCLUDES := $(wificond_includes)
 LOCAL_CPPFLAGS := $(wificond_cpp_flags)
 LOCAL_SRC_FILES := \
     ipc_constants.cpp \
+    rtt/rtt_params.cpp \
     aidl/android/net/wifi/IApInterface.aidl \
     aidl/android/net/wifi/IClientInterface.aidl \
     aidl/android/net/wifi/IInterfaceEventCallback.aidl \
+    aidl/android/net/wifi/IRttEventCallback.aidl \
     aidl/android/net/wifi/IWificond.aidl
 LOCAL_SHARED_LIBRARIES := \
     libbinder
