1618636ec906e8d7bba64d8d47f6483765d749fe
Ningyuan Wang <nywang@google.com>
2016-09-27 09:41:12 -0700

    Add Rtt interfaces to wificond part 1
    This adds a parcelable object RttParams, which is supposed to
    match class ParcelableRttParams in RttManager.java
    
    Later we will use ParcelableRttParams for framework to interact
    with wificond, so this is the c++ side definition of this object.
    
    This adds requestRtt() and cancelRtt() to IClientInterface.
    This also adds a dummy definition of interface IRttEventCallback.
    
    Bug: 31756849
    Test: compile
    
    Change-Id: I457c910adb8b1e2f4668cc24e87070560df5396c

--- client_interface_binder.cpp
+++ client_interface_binder.cpp
@@ -23,6 +23,7 @@
 #include "wificond/client_interface_impl.h"
 
 using android::binder::Status;
+using android::sp;
 using std::vector;
 
 namespace android {
@@ -76,6 +77,19 @@ Status ClientInterfaceBinder::getInterfaceName(std::string* out_name) {
     return Status::ok();
   }
   *out_name = impl_->GetInterfaceName();
+  return Status::ok();
+}
+
+Status ClientInterfaceBinder::requestRttRange(
+    const ::android::net::wifi::RttManager::ParcelableRttParams& rtt_params,
+    const sp<::android::net::wifi::IRttEventCallback>& callback,
+    bool* success) {
+  *success = true;
+  return Status::ok();
+}
+
+Status ClientInterfaceBinder::cancelRttRange(bool* success) {
+  *success = true;
   return Status::ok();
 }
 
--- client_interface_binder.h
+++ client_interface_binder.h
@@ -46,6 +46,11 @@ class ClientInterfaceBinder : public android::net::wifi::BnClientInterface {
   ::android::binder::Status getMacAddress(
       std::vector<uint8_t>* out_mac_address) override;
   ::android::binder::Status getInterfaceName(std::string* out_name) override;
+  ::android::binder::Status requestRttRange(
+      const ::android::net::wifi::RttManager::ParcelableRttParams& rtt_params,
+      const ::android::sp<::android::net::wifi::IRttEventCallback>& callback,
+      bool* success) override;
+  ::android::binder::Status cancelRttRange(bool* success) override;
 
  private:
   ClientInterfaceImpl* impl_;
