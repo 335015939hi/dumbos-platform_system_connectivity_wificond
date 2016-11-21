6cf8022b907d8aec0bfb0286db3441f3226924c1
Ningyuan Wang <nywang@google.com>
2016-11-21 11:22:43 -0800

    Add scan setting parcelable classes for single scan
    This CL includes:
    1. C++ side implementation of SingleScanSettings, ChannelSettings,
     and HiddenNetwork classes.
    
    2. Unit tests for these classes.
    
    3. Move defination of RETURN_IF_FAILED macro a single file.
    
    Bug: 33011588
    Test: compile, unit tests
    
    Change-Id: I90a32bd7fd02c4cc3cad6824434d79ec0c2b95f9

---
+++ scanning/single_scan_settings.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "wificond/scanning/single_scan_settings.h"
+
+#include <android-base/logging.h>
+
+#include "wificond/parcelable_utils.h"
+
+using namespace android::wificond::parcelable_utils;
+
+using android::status_t;
+using android::OK;
+using std::string;
+
+namespace com {
+namespace android {
+namespace server {
+namespace wifi {
+namespace wificond {
+
+status_t SingleScanSettings::writeToParcel(::android::Parcel* parcel) const {
+  RETURN_IF_FAILED(parcel->writeInt32(is_full_scan ? 1 : 0));
+  RETURN_IF_FAILED(parcel->writeInt32(channel_settings.size()));
+  for (auto& channel : channel_settings) {
+    RETURN_IF_FAILED(channel.writeToParcel(parcel));
+  }
+  RETURN_IF_FAILED(parcel->writeInt32(hidden_networks.size()));
+  for (auto& network : hidden_networks) {
+    RETURN_IF_FAILED(network.writeToParcel(parcel));
+  }
+  return ::android::OK;
+}
+
+status_t SingleScanSettings::readFromParcel(const ::android::Parcel* parcel) {
+  is_full_scan = (parcel->readInt32() != 0);
+  int32_t num_channels = 0;
+  RETURN_IF_FAILED(parcel->readInt32(&num_channels));
+  for (int i = 0; i < num_channels; i++) {
+    ChannelSettings channel;
+    RETURN_IF_FAILED(channel.readFromParcel(parcel));
+    channel_settings.push_back(channel);
+  }
+  int32_t num_hidden_networks = 0;
+  RETURN_IF_FAILED(parcel->readInt32(&num_hidden_networks));
+  for (int i = 0; i < num_hidden_networks; i++) {
+    HiddenNetwork network;
+    RETURN_IF_FAILED(network.readFromParcel(parcel));
+    hidden_networks.push_back(network);
+  }
+  return ::android::OK;
+}
+
+}  // namespace wificond
+}  // namespace wifi
+}  // namespace server
+}  // namespace android
+}  // namespace com
---
+++ tests/scan_settings_unittest.cpp
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2016, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "wificond/scanning/channel_settings.h"
+#include "wificond/scanning/hidden_network.h"
+#include "wificond/scanning/single_scan_settings.h"
+
+using ::com::android::server::wifi::wificond::ChannelSettings;
+using ::com::android::server::wifi::wificond::HiddenNetwork;
+using ::com::android::server::wifi::wificond::SingleScanSettings;
+using std::vector;
+
+namespace android {
+namespace wificond {
+
+namespace {
+
+const uint8_t kFakeSsid[] =
+    {'G', 'o', 'o', 'g', 'l', 'e', 'G', 'u', 'e', 's', 't'};
+const bool kFakeIsFullScan = false;
+constexpr uint32_t kFakeFrequency = 5260;
+constexpr uint32_t kFakeFrequency1 = 2460;
+constexpr uint32_t kFakeFrequency2 = 2500;
+
+}  // namespace
+
+class ScanSettingsTest : public ::testing::Test {
+ public:
+  // Helper functions for testing if two parcelable objects are the same
+  // as expected.
+  void expect_equal(HiddenNetwork& network1, HiddenNetwork& network2) {
+    EXPECT_EQ(network1.ssid, network2.ssid);
+  }
+
+  void expect_equal(ChannelSettings& channel1, ChannelSettings& channel2) {
+    EXPECT_EQ(channel1.frequency, channel2.frequency);
+  }
+
+  void expect_equal(SingleScanSettings& scan1, SingleScanSettings& scan2) {
+    EXPECT_EQ(scan1.is_full_scan, scan2.is_full_scan);
+    EXPECT_EQ(scan1.channel_settings.size(), scan2.channel_settings.size());
+    for (unsigned int i = 0; i < scan1.channel_settings.size(); i++) {
+      expect_equal(scan1.channel_settings[i], scan2.channel_settings[i]);
+    }
+    EXPECT_EQ(scan1.hidden_networks.size(), scan2.hidden_networks.size());
+    for (unsigned int i = 0; i < scan1.hidden_networks.size(); i++) {
+      expect_equal(scan1.hidden_networks[i], scan2.hidden_networks[i]);
+    }
+  }
+};
+
+TEST_F(ScanSettingsTest, HiddenNetworkParcelableTest) {
+  HiddenNetwork hidden_network;
+  std::vector<uint8_t> ssid(kFakeSsid, kFakeSsid + sizeof(kFakeSsid));
+  hidden_network.ssid = ssid;
+  Parcel parcel;
+  EXPECT_EQ(::android::OK, hidden_network.writeToParcel(&parcel));
+
+  HiddenNetwork hidden_network_copy;
+  parcel.setDataPosition(0);
+  EXPECT_EQ(::android::OK, hidden_network_copy.readFromParcel(&parcel));
+
+  expect_equal(hidden_network, hidden_network_copy);
+}
+
+TEST_F(ScanSettingsTest, ChannelSettingsParcelableTest) {
+  ChannelSettings channel_settings;
+  channel_settings.frequency = kFakeFrequency;
+  Parcel parcel;
+  EXPECT_EQ(::android::OK, channel_settings.writeToParcel(&parcel));
+
+  ChannelSettings channel_settings_copy;
+  parcel.setDataPosition(0);
+  EXPECT_EQ(::android::OK, channel_settings_copy.readFromParcel(&parcel));
+
+  expect_equal(channel_settings, channel_settings_copy);
+}
+
+TEST_F(ScanSettingsTest, SingleScanSettingsParcelableTest) {
+  SingleScanSettings scan_settings;
+  scan_settings.is_full_scan = kFakeIsFullScan;
+
+  ChannelSettings channel, channel1, channel2;
+  channel.frequency= kFakeFrequency;
+  channel1.frequency= kFakeFrequency1;
+  channel2.frequency= kFakeFrequency2;
+  scan_settings.channel_settings.push_back(channel);
+  scan_settings.channel_settings.push_back(channel1);
+  scan_settings.channel_settings.push_back(channel2);
+  Parcel parcel;
+  EXPECT_EQ(::android::OK, scan_settings.writeToParcel(&parcel));
+
+  SingleScanSettings scan_settings_copy;
+  parcel.setDataPosition(0);
+  EXPECT_EQ(::android::OK, scan_settings_copy.readFromParcel(&parcel));
+
+  expect_equal(scan_settings, scan_settings_copy);
+}
+
+
+}  // namespace wificond
+}  // namespace android
