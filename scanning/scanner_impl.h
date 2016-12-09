d09bb56367bfbfabe07ab4118b1e69f8f72c2f7f
Ningyuan Wang <nywang@google.com>
2016-12-09 11:35:00 -0800

    Add getScanResults() to wificond scanner
    Bug: 33398008
    Change-Id: I40c456f9b2c4ff4ba971cb09f2b5e3904519a250
    Test: compile, manual tests

--- scanning/scanner_impl.h
+++ scanning/scanner_impl.h
@@ -47,6 +47,9 @@ class ScannerImpl : public android::net::wifi::BnWifiScannerImpl {
   // Returns a vector of available frequencies for DFS channels.
   ::android::binder::Status getAvailableDFSChannels(
       ::std::vector<int32_t>* out_frequencies) override;
+  ::android::binder::Status getScanResults(
+      std::vector<com::android::server::wifi::wificond::NativeScanResult>*
+          out_scan_results) override;
   void Invalidate() { valid_ = false; }
 
  private:
