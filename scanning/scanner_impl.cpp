3966eaa7f9f1fa7f0fa5e424a1060d137a1a8cc0
Ningyuan Wang <nywang@google.com>
2016-12-09 11:53:12 -0800

    Add scan() to wificond scanner
    Bug: 33398008
    Change-Id: Ib2009547ccc8fc368bd92e88a216b61cefe9a8ad
    Test: compile

--- scanning/scanner_impl.cpp
+++ scanning/scanner_impl.cpp
@@ -16,6 +16,7 @@
 
 #include "wificond/scanning/scanner_impl.h"
 
+#include <string>
 #include <vector>
 
 #include <android-base/logging.h>
@@ -23,7 +24,9 @@
 #include "wificond/scanning/scan_utils.h"
 
 using android::binder::Status;
+using android::String16;
 using com::android::server::wifi::wificond::NativeScanResult;
+using std::string;
 using std::vector;
 
 namespace android {
@@ -85,6 +88,26 @@ Status ScannerImpl::getScanResults(vector<NativeScanResult>* out_scan_results) {
   if (!scan_utils_->GetScanResult(interface_index_, out_scan_results)) {
     LOG(ERROR) << "Failed to get scan results from NL80211";
   }
+  return Status::ok();
+}
+
+Status ScannerImpl::scan(const vector<int32_t>& freqs,
+                         const vector<String16>& ssids,
+                         bool* out_success) {
+  vector<vector<uint8_t>> raw_ssids;
+  // Add an empty ssid for a wild card scan.
+  raw_ssids.push_back({0});
+  for (auto& ssid : ssids) {
+    string ssid_str = String16::std_string(ssid);
+    raw_ssids.emplace_back(vector<uint8_t>(ssid_str.begin(), ssid_str.end()));
+  }
+  vector<uint32_t> freqs_converted(freqs.begin(), freqs.end());
+  if (!scan_utils_->Scan(interface_index_, raw_ssids, freqs_converted)) {
+    *out_success = false;
+    LOG(ERROR) << "Failed to start scan";
+    return Status::ok();
+  }
+  *out_success = true;
   return Status::ok();
 }
 
