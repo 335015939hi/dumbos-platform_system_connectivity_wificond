14feaa3d7f5cdf54f0737e1e85674cff434f0365
Sohani Rao <sohanirao@google.com>
2016-12-21 20:34:27 -0800

    Wificond: Client for Offload HAL HIDL service
    This CL creates the client side implementation for Offload HAL and
    exports it's API's to wificond. It also creates the interface object
    for callback from the Offload HAL.
    
    Bug: 32842314
    Test: Unit tests and Mannual tests of verifying Wifi can be turned on
    connected and ensure we are getting a handle on Offload HAL service
    Change-Id: Ie63b31742a6b53d60c4c2a3560829fc69a2a5f3d

---
+++ scanning/offload/offload_scan_utils.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "wificond/scanning/offload/offload_scan_utils.h"
+#include "wificond/scanning/scan_result.h"
+
+using android::sp;
+using ::com::android::server::wifi::wificond::NativeScanResult;
+using android::hardware::wifi::offload::V1_0::implementation::OnOffloadScanResultsReadyHandler;
+using android::hardware::wifi::offload::V1_0::ScanResult;
+
+namespace android {
+namespace wificond {
+
+android::sp<IOffload> OffloadScanUtils::getOffloadService() {
+  return IOffload::getService();	
+}
+
+android::sp<OffloadCallback> OffloadScanUtils::getOffloadCallback(OnOffloadScanResultsReadyHandler handler) {
+  return new OffloadCallback(handler);
+}
+
+std::vector<::com::android::server::wifi::wificond::NativeScanResult> OffloadScanUtils::convertToNativeScanResults(const std::vector<ScanResult> scanResult) {
+  std::vector<::com::android::server::wifi::wificond::NativeScanResult> nativeScanResult;
+  nativeScanResult.reserve(scanResult.size());
+  for (size_t i = 0; i < scanResult.size(); i++) {
+    NativeScanResult *singleScanResult = new NativeScanResult();
+    singleScanResult->ssid = scanResult[i].networkInfo.ssid;
+    singleScanResult->bssid.reserve(6);
+    memcpy(&singleScanResult->bssid[0], &scanResult[i].bssid[0], 6);
+    singleScanResult->frequency = scanResult[i].frequency;
+    singleScanResult->signal_mbm = scanResult[i].rssi;
+    singleScanResult->tsf = scanResult[i].tsf;
+    singleScanResult->capability = scanResult[i].capability;
+    singleScanResult->associated = false;
+    nativeScanResult.push_back(*singleScanResult);
+  }
+  return nativeScanResult;
+}
+
+} // wificond
+} // android
+
---
+++ scanning/offload/offload_scan_utils.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef WIFICOND_OFFLOAD_SCAN_UTILS_H_
+#define WIFICOND_OFFLOAD_SCAN_UTILS_H_
+
+#include <android/hardware/wifi/offload/1.0/IOffload.h>
+#include "wificond/scanning/offload/OffloadCallback.h"
+
+#include <vector>
+
+using android::hardware::wifi::offload::V1_0::IOffload;
+using android::hardware::wifi::offload::V1_0::implementation::OffloadCallback;
+using android::hardware::wifi::offload::V1_0::implementation::OnOffloadScanResultsReadyHandler;
+using android::hardware::wifi::offload::V1_0::ScanResult;
+
+namespace com {
+namespace android {
+namespace server {
+namespace wifi {
+namespace wificond {
+
+class NativeScanStats;
+
+}  // namespace wificond
+}  // namespace wifi
+}  // namespace server
+}  // namespace android
+}  // namespace com
+
+namespace android {
+namespace wificond {
+
+// Provides methods to get Offload HAL service and create callback
+class OffloadScanUtils {
+ public:
+  static android::sp<IOffload> getOffloadService();
+  static android::sp<OffloadCallback> getOffloadCallback(OnOffloadScanResultsReadyHandler handler);
+  static std::vector<::com::android::server::wifi::wificond::NativeScanResult> convertToNativeScanResults(const std::vector<ScanResult>);
+};
+
+}  // namespace wificond
+}  // namespace android
+
+#endif // WIFICOND_OFFLOAD_SCAN_UTILS_H
+
