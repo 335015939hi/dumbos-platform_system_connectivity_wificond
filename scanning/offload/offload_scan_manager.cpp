5cf0a705e1786f392ee2df46779d47528e77af59
Sohani Rao <sohanirao@google.com>
2016-12-21 20:34:27 -0800

    Wificond: Client for Offload HAL HIDL service
    This CL creates the client side implementation for Offload HAL and
    exports it's API's to wificond. It also creates the interface object
    for callback from the Offload HAL.
    
    Bug: 32842314
    Test: Unit tests and Mannual tests of verifying Wifi can be turned on
    connected and ensure we are getting a handle on Offload HAL service
    Change-Id: Ie63b31742a6b53d60c4c2a3560829fc69a2a5f3d

---
+++ scanning/offload/offload_scan_manager.cpp
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "wificond/scanning/offload/offload_scan_manager.h"
+
+#include <vector>
+
+#include <android-base/logging.h>
+
+#include "wificond/scanning/offload/offload_scan_utils.h"
+#include "wificond/scanning/offload/offload_service_utils.h"
+#include "wificond/scanning/scan_result.h"
+
+using ::android::hardware::hidl_vec;
+using android::hardware::wifi::offload::V1_0::IOffload;
+using android::hardware::wifi::offload::V1_0::ScanResult;
+using android::hardware::wifi::offload::V1_0::implementation::OffloadCallback;
+using ::com::android::server::wifi::wificond::NativeScanResult;
+using android::wificond::OnNativeScanResultsReadyHandler;
+
+namespace android {
+namespace wificond {
+
+NativeScanResultHandler::NativeScanResultHandler(
+    OnNativeScanResultsReadyHandler handler)
+  : handler_(handler) {
+}
+
+void NativeScanResultHandler::callback(
+    const std::vector<ScanResult> &scanResult) {
+  std::vector<NativeScanResult> scanResult_;
+  scanResult_ = OffloadScanUtils::convertToNativeScanResults(scanResult);
+  handler_(scanResult_);
+}
+
+NativeScanResultHandler::~NativeScanResultHandler() {}
+
+OffloadScanManager::OffloadScanManager(OffloadServiceUtils *utils,
+    OnNativeScanResultsReadyHandler handler)
+    : scan_result_handler_(new NativeScanResultHandler(handler)) {
+  wifi_offload_hal_ = utils->getOffloadService();
+  if (wifi_offload_hal_ == nullptr) {
+    service_available_ = false;
+    LOG(ERROR) << "Failed to get Offload service";
+    return;
+  }
+  service_available_ = true;
+  wifi_offload_callback_ = utils->getOffloadCallback(
+      ([this] (std::vector<ScanResult> scanResult) -> void {
+        this->reportScanResults(scanResult);
+      }));
+  wifi_offload_hal_->setEventCallback(wifi_offload_callback_);
+}
+
+OffloadScanManager::~OffloadScanManager() {}
+
+void OffloadScanManager::reportScanResults(
+    const std::vector<ScanResult> scanResult) {
+  scan_result_handler_->callback(scanResult);
+}
+
+bool OffloadScanManager::isServiceAvailable() {
+  return service_available_;
+}
+
+}  // namespace wificond
+}  // namespace android
