3b9682a9f3e958c9e7e3eafb5f91ee20c9595470
Sohani Rao <sohanirao@google.com>
2017-03-07 18:32:56 -0800

    Wificond: Unit tests for Offload HAL calling code
    This CL creates mock objects and unit tests for the new classes
    introduces that interact with Offload HAL
    
    Bug: 32842314
    Test: Unit tests and Mannual tests of verifying Wifi can be turned on
    connected and ensure we are getting a handle on Offload HAL service
    
    Change-Id: Idbbf7f67c8e0e1cec04c573225c36a3d5a4479af

---
+++ tests/mock_offload_callback.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "wificond/tests/mock_offload_callback.h"
+#include "wificond/scanning/scan_result.h"
+
+using android::hardware::wifi::offload::V1_0::implementation::OnNativeScanResultsReadyHandler;
+using android::hardware::wifi::offload::V1_0::ScanResult;
+using ::android::hardware::hidl_vec;
+
+namespace android {
+namespace wificond {
+
+MockOffloadCallback::MockOffloadCallback(OnNativeScanResultsReadyHandler handler) :
+    OffloadCallback(handler), handler_(handler) { 
+}
+
+void MockOffloadCallback::injectScanResults(std::vector<ScanResult> scanResult) {
+  hidl_vec<ScanResult> hidl_scan_result(scanResult);
+  std::vector<::com::android::server::wifi::wificond::NativeScanResult> native_scan_result;
+  native_scan_result.reserve(scanResult.size());
+  handler_(native_scan_result);
+}
+
+} // wificond
+} // android
---
+++ tests/mock_offload_callback.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef WIFICOND_TEST_MOCK_OFFLOAD_CALLBACK_H
+#define WIFICOND_TEST_MOCK_OFFLOAD_CALLBACK_H
+
+#include <gmock/gmock.h>
+#include <android/hardware/wifi/offload/1.0/IOffloadCallback.h>
+#include "wificond/scanning/offload/OffloadCallback.h"
+#include <vector>
+using android::hardware::wifi::offload::V1_0::IOffloadCallback;
+using android::hardware::wifi::offload::V1_0::ScanResult;
+using ::android::hardware::hidl_vec;
+using android::hardware::wifi::offload::V1_0::implementation::OffloadCallback;
+using android::hardware::wifi::offload::V1_0::implementation::OnNativeScanResultsReadyHandler;
+
+namespace android {
+namespace wificond {
+
+class MockOffloadCallback : public OffloadCallback {
+  public:
+    MockOffloadCallback(OnNativeScanResultsReadyHandler handler);
+    ~MockOffloadCallback() override = default;
+
+    MOCK_METHOD1(onScanResult, ::android::hardware::Return<void>(const hidl_vec<ScanResult> &scanResult));
+    void injectScanResults(std::vector<ScanResult> scanResult);
+  private:
+    OnNativeScanResultsReadyHandler handler_;
+};
+
+} // wificond
+} // android
+
+#endif // WIFICOND_TEST_MOCK_OFFLOAD_CALLBACK_H
